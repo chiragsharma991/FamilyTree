4370405f30d18069d24616ff8e60c5cc5828c807
chari <chiragsharma991@gmail.com>
2016-09-02 19:03:01 +0530

    Initial commit

--- app/src/main/java/demo/test/com/test/Second.java
+++ app/src/main/java/demo/test/com/test/Second.java
@@ -3,11 +3,16 @@ package demo.test.com.test;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 
 /**
@@ -15,17 +20,22 @@ import android.widget.ScrollView;
  */
 public class Second extends AppCompatActivity
 {
-    private LinearLayout linearLayout;
+    private RelativeLayout relativeLayout;
     private View addView;
+    private RelativeLayout.LayoutParams params1;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
 
         setContentView(R.layout.test);
         super.onCreate(savedInstanceState);
-         linearLayout=(LinearLayout)findViewById(R.id.layout);
+         relativeLayout=(RelativeLayout) findViewById(R.id.layout);
        // ScrollView scrollView=(ScrollView)findViewById(R.id.scroll);
        // scrollView.setVerticalScrollBarEnabled(true);
+         params1=new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,RelativeLayout.LayoutParams.WRAP_CONTENT);
+        ImageView imageView=(ImageView)findViewById(R.id.yyyy);
+        params1.addRule(RelativeLayout.BELOW,imageView.getId());
+        params1.addRule(RelativeLayout.START_OF,imageView.getId());
 
 
 
@@ -37,9 +47,44 @@ public class Second extends AppCompatActivity
     {
         LayoutInflater layoutInflater=(LayoutInflater)getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         addView=layoutInflater.inflate(R.layout.raw,null,false);
+        ImageView imageView1=(ImageView)addView.findViewById(R.id.image1);
+        ImageView imageView2=(ImageView)addView.findViewById(R.id.image2);
+        imageView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.e("click on under 1","log");
+                LayoutInflater layoutInflater=(LayoutInflater)getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                addView=layoutInflater.inflate(R.layout.raw,null,false);
+                params1.addRule(RelativeLayout.BELOW,view.getId());
+                params1.addRule(RelativeLayout.END_OF,view.getId());
+                relativeLayout.addView(addView,params1);
+
+                // ImageView imageView1=(ImageView)addView.findViewById(R.id.image1);
+              //  ImageView imageView2=(ImageView)addView.findViewById(R.id.image2);
+
+            }
+        });
+
+        imageView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.e("click on under 2","log");
+                LayoutInflater layoutInflater=(LayoutInflater)getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                addView=layoutInflater.inflate(R.layout.raw,null,false);
+                //ImageView imageView1=(ImageView)addView.findViewById(R.id.image1);
+                //ImageView imageView2=(ImageView)addView.findViewById(R.id.image2);
+
+                params1.addRule(RelativeLayout.BELOW,view.getId());
+                params1.addRule(RelativeLayout.END_OF,view.getId());
+                relativeLayout.addView(addView,params1);
+
+            }
+        });
+
+
 
         Log.e("on click","log");
-        linearLayout.addView(addView);
+        relativeLayout.addView(addView,params1);
 
 
 
